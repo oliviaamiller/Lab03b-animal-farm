ae3869554ca953e87ec903cb7c089c24f113f5b7
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-11-04 14:39:16 -0700

    js working for noises and which animal last made a noise

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const dogSound = document.getElementById('woof');
 const dogImage = document.getElementById('dog');
 
+
 const catSound = document.getElementById('meow');
 const catImage = document.getElementById('cat');
+
 
 const horseSound = document.getElementById('neigh');
 const horseImage = document.getElementById('horse');
 
+const whoSpokeLast = document.getElementById('who-spoke');
+
+
+// let catClicks = 0;
+// let dogClicks = 0;
+// let horseClicks = 0;
+
+
 
 dogImage.addEventListener('click', () => {
-  dogSound.play();
+    dogSound.play();
+    whoSpokeLast.textContent = 'Dog Barked';
 });
 
 catImage.addEventListener('click', () => {
-  catSound.play();
+    catSound.play();
+    whoSpokeLast.textContent = 'Cat Meowed';
+
 });
 
 horseImage.addEventListener('click', () => {
-  horseSound.play();
+    horseSound.play();
+    whoSpokeLast.textContent = 'Horse Neighed';
 });
